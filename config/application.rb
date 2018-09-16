817c02f70aee4bfd275ba444911a44ca359df02c
yousuke-shiraishi <y-shiraishi@nexyzbb.ne.jp>
2018-09-16 12:43:10 +0900

    日本時間に対応

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module Instagram
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    #-----------------------------------
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    #-----------------------------------
     config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializer
