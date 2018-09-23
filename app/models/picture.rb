a12b9da5f8a4b3108b2b5a663e2cdf972379b448
yousuke-shiraishi <y-shiraishi@nexyzbb.ne.jp>
2018-09-23 13:07:06 +0900

    fixed priceにバリデーションをかけた

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,6 +2,7 @@ class Picture < ApplicationRecord
   validates :title, presence: true, length:{minimum: 1, maximum: 25}
   validates :content, presence: true, length:{minimum: 1, maximum: 140}
   validates :image, presence: true
+  validates :price, presence: true, numericality:{only_integer: true}
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   belongs_to :user
